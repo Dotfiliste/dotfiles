9295430bb37f676e6a3c4d1cbe5d634e3d0b18fd
Dotfiliste <dotfiliste@chezmoi>
2025-04-26 11:07:59 +0200

    use better variable for cd

--- .chezmoiscripts/run_once_init.sh
+++ .chezmoiscripts/run_once_init.sh
@@ -1,6 +1,6 @@
 #! /bin/bash
 
-cd ..
+cd $CHEZMOI_SOURCE_DIR
 git config user.name Dotfiliste
 git config user.email dotfiliste@chezmoi
 git config --add --local core.sshCommand 'ssh -i ~/.ssh/dotfiles'
