a921d6ae4a79d0547c748a8cd621aa5b8fadd492
Dotfiliste <dotfiliste@chezmoi>
2025-04-26 10:30:22 +0200

    update init script

--- .chezmoiscripts/run_once_init.sh
+++ .chezmoiscripts/run_once_init.sh
@@ -4,3 +4,4 @@ chezmoi cd
 git config user.name Dotfiliste
 git config user.email dotfiliste@chezmoi
 git config --add --local core.sshCommand 'ssh -i ~/.ssh/dotfiles'
+git remote set-url origin git@github.com:dotfiliste/dotfiles.git
