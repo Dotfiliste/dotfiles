911feb2045acd70bc7566dcab95bfe439862c207
Dotfiliste <dotfiliste@chezmoi>
2025-09-18 15:43:01 +0200

    revert remote url

--- .chezmoiscripts/run_once_init.sh
+++ .chezmoiscripts/run_once_init.sh
@@ -4,4 +4,4 @@ cd $CHEZMOI_SOURCE_DIR
 git config user.name Dotfiliste
 git config user.email dotfiliste@chezmoi
 git config --add --local core.sshCommand 'ssh -i ~/.ssh/dotfiles'
-git remote set-url origin github.com:dotfiliste/dotfiles.git
+git remote set-url origin git@github.com:dotfiliste/dotfiles.git
