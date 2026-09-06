e11a09a836f7106e9be0fcfdb244d90201190b10
Dotfiliste <dotfiliste@chezmoi>
2026-09-06 17:58:47 +0200

    fix: add eval when installing

--- .chezmoiscripts/run_once_after_install_50-brew.sh
+++ .chezmoiscripts/run_once_after_install_50-brew.sh
@@ -5,6 +5,7 @@ if [ "$?" != 0 ]
 then
     echo "$0: homebrew not found, installing"
     /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+    eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
 fi
 echo "$0: homebrew found, installing dependencies"
 
