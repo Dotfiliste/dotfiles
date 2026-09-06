5fd7ad6276c1a337fb17ff603b1d6d00d0f9f679
Dotfiliste <dotfiliste@chezmoi>
2026-09-06 16:46:03 +0200

    updates scripts

--- .chezmoiscripts/run_once_after_install_50-brew.sh
+++ .chezmoiscripts/run_once_after_install_50-brew.sh
@@ -1,6 +1,12 @@
 #! /bin/bash
 
-/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+which brew
+if [ "$?" != 0 ]
+then
+    echo "$0: homebrew not found, installing"
+    /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+fi
+echo "$0: homebrew found, installing dependencies"
 
 brew bundle --file=/dev/stdin <<EOF
 brew "fzf"
@@ -8,5 +14,4 @@ brew "keychain"
 brew "neovim"
 brew "pure"
 brew "zoxide"
-brew "font-jetbrains-mono-nerd-font"
 EOF
