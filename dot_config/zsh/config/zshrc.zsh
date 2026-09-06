ca6a931f41ef6cf7bd6ca791ad0967a64c3c740c
Dotfiliste <dotfiliste@chezmoi>
2026-09-06 15:13:09 +0200

    remove template and fix keybinds

--- dot_config/zsh/config/zshrc.zsh
+++ dot_config/zsh/config/zshrc.zsh
@@ -74,13 +74,8 @@ compinit
 autoload -Uz history-search-end
 zle -N history-beginning-search-backward-end history-search-end
 zle -N history-beginning-search-forward-end history-search-end
-{{- if eq .chezmoi.os "darwin" }}
 bindkey "^[[A" history-beginning-search-backward-end
 bindkey "^[[B" history-beginning-search-forward-end
-{{- else }}
-bindkey "^[OA" history-beginning-search-backward-end
-bindkey "^[OB" history-beginning-search-forward-end
-{{- end }}
 
 # Keychain
 eval $(keychain --eval --quiet)
